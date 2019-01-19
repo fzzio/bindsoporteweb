fc24eeecfedb633192fe15b284111ba6304351cd
Fabricio Orrala <fabricio@cajanegra.com.ec>
2019-01-18 23:04:55 -0500

    Colores y fondos

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BIND :: Drone Industry</title>
+    <meta name="description" content="Demo of A Free Coming Soon Bootstrap 4 Template by TemplateFlip.com."/>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="styles/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome-animation.min.css">
+    <link rel="stylesheet" href="styles/bindsoporte.css">
+  </head>
+  <body id="top">
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="inner cover clearfix">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-md-8">
+                  <h1 class="text-center txt-blanco text-uppercase">
+                    Espéralo
+                  </h1>
+                </div>
+                <div class="col-md-4">
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <img src="img/bindsoporte-logo.png" alt="Bind Soporte" class="img-logobind mx-auto d-block" />
+                      </div>
+                    </div>
+                    <div class="row mt-2">
+                      <div class="col-md-12">
+                        <nav class="nav justify-content-center">
+                          <a class="nav-link nav-social nav-social-bind" href="https://www.facebook.com/binddrone/" target="_blank">
+                            <i class="fa fa-facebook faa-wrench animated-hover" aria-hidden="true"></i>
+                          </a>
+                          <a class="nav-link nav-social nav-social-bind" href="https://www.instagram.com/binddrone/" target="_blank">
+                            <i class="fa fa-instagram faa-wrench animated-hover" aria-hidden="true"></i>
+                          </a>
+                          <a class="nav-link nav-social nav-social-bind" href="https://api.whatsapp.com/send?phone=593983992784&text=Hola%20estoy%20interesado%20en%20los%20servicios%20de%20Bind" target="_blank">
+                            <i class="fa fa-whatsapp faa-wrench animated-hover" aria-hidden="true"></i>
+                          </a>
+                          <a class="nav-link nav-social nav-social-bind" href="#" target="_blank">
+                            <i class="fa fa-youtube faa-wrench animated-hover" aria-hidden="true"></i>
+                          </a>
+                          <a class="nav-link nav-social nav-social-bind" href="#" target="_blank">
+                            <i class="fa fa-google-plus faa-wrench animated-hover" aria-hidden="true"></i>
+                          </a>
+                        </nav>
+                      </div>
+                    </div>
+                    <div class="row mt-3">
+                      <div class="col-sm-8 offset-sm-2">
+                          <span class="text-uppercase font-weight-bold">Información</span>
+                          <hr class="divider-style m-0" />
+                          <span class="font-weight-bold">Richard Moreno</span>
+                          <br />
+                          <span class="">Telf: (593)98399-2784</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="mastfoot">
+            <div class="inner">
+              <p class="txt-blanco">&copy; <?php echo date('Y'); ?> Bind Soporte. Todos los derechos reservados.</p>
+            </div>
+          </div>
+          <div class="modal fade" id="subscribeModal" tabindex="-1" role="dialog" aria-labelledby="subscribeModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="subscribeModalLabel">Get Notified on Launch:</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form>
+                    <div class="form-group">
+                      <label for="recipient-name" class="form-control-label">Enter you e-mail to get notified when we launch</label>
+                      <input type="text" class="form-control" id="recipient-name" placeholder="your-name@example.com">
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default">Subscribe</button>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="scripts/main.js"></script>
+  </body>
+</html>
